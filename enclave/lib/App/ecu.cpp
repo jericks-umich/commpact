f5b0d2e5b3c8e454ce55c2a5ce7551938add3018
Mel Savich <msavich@umich.edu>
2018-02-11 12:58:52 -0500

    rough declaration of ecu

---
+++ enclave/lib/App/ecu.cpp
@@ -0,0 +1,21 @@
+#include "ecu.h"
+
+commpact_status_t initializeKeysECU(cp_ec256_public_t *pubkey) {
+  return CP_SUCCESS;
+}
+
+commpact_status_t setInitialPositionECU(cp_ec256_signature_t *signature,
+                                        int position) {
+  return CP_SUCCESS;
+}
+
+commpact_status_t setInitialSpeedBoundsECU(cp_ec256_signature_t *signature,
+                                           double lower, double upper) {
+  return CP_SUCCESS;
+}
+
+commpact_status_t
+setInitialRecoveryPhaseTimeoutECU(cp_ec256_signature_t *signature,
+                                  double timeout) {
+  return CP_SUCCESS;
+}
--- enclave/lib/App/ecu.h
+++ enclave/lib/App/ecu.h
@@ -1,4 +1,18 @@
 #ifndef COMMPACT_ECU_H
 #define COMMPACT_ECU_H
 
+#include "sgx_tcrypto.h"
+
+#include "../include/commpact_types.h"
+
+// setters
+commpact_status_t initializeKeysECU(cp_ec256_public_t *pubkey);
+commpact_status_t setInitialPositionECU(cp_ec256_signature_t *signature,
+                                        int position);
+commpact_status_t setInitialSpeedBoundsECU(cp_ec256_signature_t *signature,
+                                           double lower, double upper);
+commpact_status_t
+setInitialRecoveryPhaseTimeoutECU(cp_ec256_signature_t *signature,
+                                  double timeout);
+
 #endif // COMMPACT_ECU_H

be9efa53b09a7fcb1c56252a872d5a5cc4c5d2db
Shibo <chshibo@umich.edu>
2018-04-26 15:14:58 -0400

    Fixed break in enclave.cpp

--- enclave/lib/Enclave/Enclave.cpp
+++ enclave/lib/Enclave/Enclave.cpp
@@ -299,11 +299,12 @@ sgx_status_t validateSignaturesHelper(contract_chain_t *contract,
     return SGX_SUCCESS;
   }
   int retval = 0;
-  for (int i = 0; i < contract->chain_length; ++i) {
+  for (int i = 0; i < num_signatures; ++i) {
     // i is the position in the chain_order
     uint8_t pos = contract->chain_order[i];
-    if (pos == position) { // if we've reached our own vehicle
-      break;               // then we're done verifying signatures
+    if (pos == position) { // if we've reached our own vehicle  then we're done
+                           // verifying signatures
+      continue;
     }
     if (num_signatures < pos) { // sanity check
       ocallPrintI(&retval, (int)num_signatures);
